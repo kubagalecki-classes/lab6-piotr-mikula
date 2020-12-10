01933b1c2699deda99d776589159576e31c155b1
piotrmikula <p.mikula14@wp.pl>
2020-12-10 15:46:09 +0000

    Cwiczenie_III

--- lab6.cpp
+++ lab6.cpp
@@ -5,19 +5,12 @@
 int main()
 {
     std::string haslo;
-    std::string klucz1 = "piesek";
-    std::string klucz2 = "kotek";
 
     std::cout << "Podaj ciag znakow: ";
     std::cin >> haslo;
 
-    auto x1 = std::search(haslo.begin(), haslo.end(), klucz1.begin(), klucz1.end());
-    auto x2 = std::search(haslo.begin(), haslo.end(), klucz2.begin(), klucz2.end());
-
-    if ((*x1 != 0) || (*x2 != 0))
-        std::cout << "1 - Znaleziono podany ciag\n";
-    else
-        std::cout << "0 - Nie znaleziono podanego ciagu\n";
+    std::reverse(haslo.begin(), haslo.end());
+    std::cout << "Ciag znakow po odwroceniu: " << haslo << std::endl;
 
     puts("\nOstatnia linijka kodu!");
 }
