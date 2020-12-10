cf16a219ee0750c5a97abf2b14e0ab73628aa1fc
piotrmikula <p.mikula14@wp.pl>
2020-12-10 02:44:33 +0000

    Zadanie1

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,17 @@
+#include "make_random_vector.hpp"
 #include <iostream>
 
 int main()
 {
-    puts("Hello, World!");
+    std::vector< int > vec;
+    vec = make_random_vector(6, 0, 10);
+    for (auto it = vec.begin(); it != vec.end(); it++)
+        std::cout << *it << ",\t";
+
+    std::cout << "\n\nSortowanie wartosci w wektorze\n";
+    std::sort(vec.begin(), vec.end());
+    for (auto it = vec.begin(); it != vec.end(); it++)
+        std::cout << *it << ",\t";
+
+    puts("\nOstatnia linijka kodu!");
 }
