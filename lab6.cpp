d5654b4f08e7b57ffc740a65b2248bfa5d742616
piotrmikula <p.mikula14@wp.pl>
2020-12-10 03:03:26 +0000

    Zadanie4

--- lab6.cpp
+++ lab6.cpp
@@ -3,20 +3,23 @@
 
 struct myclass
 {
-    bool operator()(int i, int j) { return (i > j); }
-} myobject;
+    bool operator()(int i) { return (i > 7); }
+} isBigger;
 
 int main()
 {
     std::vector< int > vec;
-    vec = make_random_vector(10, 0, 20);
+    vec = make_random_vector(10, 0, 10);
     for (auto it = vec.begin(); it != vec.end(); it++)
         std::cout << *it << ",\t";
 
-    std::cout << "\n\nSortowanie malejaco wartosci w wektorze\n";
-    std::sort(vec.begin(), vec.end(), myobject);
+    std::cout << "\n\nSortowanie rosnaco wartosci w wektorze\n";
+    std::sort(vec.begin(), vec.end());
     for (auto it = vec.begin(); it != vec.end(); it++)
         std::cout << *it << ",\t";
+
+    int licznik = count_if(vec.begin(), vec.end(), isBigger);
+    std::cout << "\n\nLiczba wieksza niz 7 wystepuje " << licznik << " razy\n";
 
     puts("\nOstatnia linijka kodu!");
 }
