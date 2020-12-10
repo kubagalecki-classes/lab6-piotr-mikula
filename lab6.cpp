441c83900e589caeb9ff43418dd2bcc226a6fe1e
piotrmikula <p.mikula14@wp.pl>
2020-12-10 15:45:51 +0000

    Cwiczenie_II

--- lab6.cpp
+++ lab6.cpp
@@ -5,16 +5,19 @@
 int main()
 {
     std::string haslo;
+    std::string klucz1 = "piesek";
+    std::string klucz2 = "kotek";
 
     std::cout << "Podaj ciag znakow: ";
     std::cin >> haslo;
 
-    auto x = std::adjacent_find(haslo.begin(), haslo.end());
-    
-    if (*x != 0)
-        std::cout << "1 - Wystepuja dwa te same znaki pod rzad\n";
+    auto x1 = std::search(haslo.begin(), haslo.end(), klucz1.begin(), klucz1.end());
+    auto x2 = std::search(haslo.begin(), haslo.end(), klucz2.begin(), klucz2.end());
+
+    if ((*x1 != 0) || (*x2 != 0))
+        std::cout << "1 - Znaleziono podany ciag\n";
     else
-        std::cout << "0 - Nie wystepuja dwa te same znaki pod rzad\n";
+        std::cout << "0 - Nie znaleziono podanego ciagu\n";
 
     puts("\nOstatnia linijka kodu!");
 }
