6641f4692a4b07621a196232d882ce27dbf4b560
piotrmikula <p.mikula14@wp.pl>
2020-12-11 14:38:42 +0000

    Cwiczenie_VIII

--- lab6.cpp
+++ lab6.cpp
@@ -5,16 +5,15 @@
 int main()
 {
     std::vector< int > vec;
-    vec = make_random_vector(10, 0, 10);
+    vec = make_random_vector(8, -1, 1);
     std::cout << "Wektor losowych liczb calkowitych z przedzialu [0, 10]\n";
     for (auto it = vec.begin(); it != vec.end(); it++)
         std::cout << *it << ",\t";
 
-    std::rotate(vec.begin(), find(vec.begin(), vec.end(), 7), vec.end());
-    std::cout << "\n\nWektor po przesunieciu\n";
-    for (auto it = vec.begin(); it != vec.end(); it++)
-        std::cout << *it << ",\t";
-    std::cout << "\n";
+    if (std::any_of(vec.begin(), vec.end(), [](int i) { return i > 0.9; }))
+        std::cout << "\n\nPrzynajmniej jeden element wektora jest wieksza niz 0.9.\n";
+    else
+        std::cout << "\n\nNie istnieje element wektora wiekszy niz 0.9.\n";
 
     puts("\nOstatnia linijka kodu!");
 }
