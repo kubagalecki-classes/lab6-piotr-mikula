9ea68acb94b8aa7940d0e3f2eaf3a1ccc1068690
piotrmikula <p.mikula14@wp.pl>
2020-12-11 15:16:54 +0000

    Cwiczenie_X

--- lab6.cpp
+++ lab6.cpp
@@ -1,23 +1,22 @@
 #include "make_random_vector.hpp"
 #include <algorithm>
-#include <cmath>
 #include <iostream>
 
 int main()
 {
-    std::vector< double > vec;
-    vec = make_random_vector(5, -1.0, 1.0);
-    std::cout << "Wektor losowych liczb zmiennoprzecinkowych z przedzialu [0, 10]\n";
+    std::vector< int > vec;
+    vec = make_random_vector(10, 0, 10);
+    
+    std::cout << "Wektor losowych liczb calkowitych z przedzialu [0, 10]\n";
     for (auto it = vec.begin(); it != vec.end(); it++)
         std::cout << *it << ",\t";
 
-    std::vector< double > vec_sin;
-    vec_sin.resize(vec.size());
+    auto x = std::partition(vec.begin(), vec.end(), [](int i) { return (i > 6); });
+    std::cout << "\n\n";
 
-    std::transform(vec.begin(), vec.end(), vec_sin.begin(), [](double i) { return sin(i); });
-
-    std::cout << "\n\nWektor sinusow wartosci z pierwszego wektora\n";
-    for (auto it = vec_sin.begin(); it != vec_sin.end(); it++)
+    std::sort(vec.begin(), x);
+    
+    for (auto it = vec.begin(); it != vec.end(); it++)
         std::cout << *it << ",\t";
 
     puts("\n\nOstatnia linijka kodu!");
